8690e79cf98eee35de3c81ed149c7106bd5396fb
karthikgrama <karthik.grama@gmail.com>
2018-02-25 00:26:54 +0100

    Add files via upload

--- pipeline/utils/visualization.py
+++ pipeline/utils/visualization.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 y,x = -87.94011,41.64454
 Y,X = -87.52413,42.02303
 coors_ll = {'low_left_x' : x, 'low_left_y' : y, 'up_right_x' : X, 'up_right_y' : Y}
-shapeFile = 'citymap/geo_export_0d18d288-fb07-4743-8d14-780bc1108034.shp'
+shapeFile = 'pipeline/utils/geo_export_0d18d288-fb07-4743-8d14-780bc1108034.shp'
 
 
 
